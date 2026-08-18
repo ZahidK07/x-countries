9aefdac4bb138823b2ec156bd9f135018a2991c9
ZahidK07 <zaksfit07@gmail.com>
2026-08-18 16:42:38 +0530

    Search feature implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,57 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
   const [countries, setCountries] = useState([]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
-    fetch("https://xcountries-backend.labs.crio.do/all")
+    fetch(
+      "https://countries-search-data-prod-812920491762.asia-south1.run.app/countries",
+    )
       .then((response) => {
         if (!response.ok) {
           throw new Error(`HTTP Error! Status: ${response.status}`);
         }
+
         return response.json();
       })
       .then((data) => {
         setCountries(data);
       })
       .catch((error) => {
-        console.error("Error fetching data:", error);
+        console.error("Error fetching countries:", error);
       });
   }, []);
 
+  const filteredCountries = countries.filter((country) =>
+    country.common.toLowerCase().includes(search.toLowerCase()),
+  );
+
   return (
-    <>
-      <div className="container">
-        {countries.map((val, i) => (
-          <div className="flag-card" key={`${val.abbr || i}-${val.name}`}>
-            <div className="flag">
-              <img src={val.flag} alt={val.name} />
-              <h4>{val.name}</h4>
-            </div>
+    <div className="app">
+      <div className="search-container">
+        <input
+          type="text"
+          value={search}
+          onChange={(event) => setSearch(event.target.value)}
+          className="search-input"
+        />
+      </div>
+
+      <div className="countries-container">
+        {filteredCountries.map((country) => (
+          <div className="countryCard" key={country.common}>
+            <img
+              src={country.png}
+              alt={country.common}
+              className="country-flag"
+            />
+            <h3>{country.common}</h3>
           </div>
         ))}
       </div>
-    </>
+    </div>
   );
 }
 
